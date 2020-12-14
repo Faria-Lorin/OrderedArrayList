4782a9938942d90168e49af7ba84a1e6e5b5be54
Faria-Lorin <florin20@stuy.edu>
2020-12-13 23:27:18 -0500

    I added the constructors, which just use the constructors for ArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,13 @@
 import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+  
   public boolean add(T element){
     if (element == null){
       throw new IllegalArgumentException("IllegalArgumentException NO nulls allowed");
