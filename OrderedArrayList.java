19e3e54b7386be78f3aa4b473e734e9993d472bc
Faria-Lorin <florin20@stuy.edu>
2020-12-14 00:12:46 -0500

    I created a private helper method to find the correct index to add to in OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,12 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
-  
+  private int sortedIndex;
+
+  private int indexFinder(T element){
+    for ( int i = 0; i < super.size() - 1; i++){
+      if ((this.get(i).compareTo(element) < 0 && this.get(i+1).compareTo(element) > 0) || (this.get(i).compareTo(element) == 0)){
+        sortedIndex = i+1;
+      }
+    }
+    return sortedIndex;
+  }
 }
