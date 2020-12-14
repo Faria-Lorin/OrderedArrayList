75cf802af92dcd5e15a70689fc9a36e5644036fd
Faria-Lorin <florin20@stuy.edu>
2020-12-14 00:28:02 -0500

    I added constuctors to OrderedArrayList, which are just like the constructors for ArrayList and NoNullList. I didn't add any code to sort the elements of the List because the list created will be empty

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,6 +1,14 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   private int sortedIndex;
 
+  public OrderedArrayList(){
+    super();
+  }
+
+  public OrderedArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+
   private void indexFinder(T element){
     for ( int i = 0; i < super.size() - 1; i++){
       if ((this.get(i).compareTo(element) < 0 && this.get(i+1).compareTo(element) > 0) || (this.get(i).compareTo(element) == 0)){
