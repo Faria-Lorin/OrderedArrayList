cd176edf2b0a87cfaa11f881b5c82402bfa4d576
Faria-Lorin <florin20@stuy.edu>
2020-12-14 00:18:07 -0500

    I made the indexFinder method return type void now, and called the function in the beginning of my add method for OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,16 +1,16 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
   private int sortedIndex;
 
-  private int indexFinder(T element){
+  private void indexFinder(T element){
     for ( int i = 0; i < super.size() - 1; i++){
       if ((this.get(i).compareTo(element) < 0 && this.get(i+1).compareTo(element) > 0) || (this.get(i).compareTo(element) == 0)){
         sortedIndex = i+1;
       }
     }
-    return sortedIndex;
   }
 
   public boolean add(T element){
+    indexFinder(element);
     add(sortedIndex, element);
     if (this.get(sortedIndex) == element){
       return true;
