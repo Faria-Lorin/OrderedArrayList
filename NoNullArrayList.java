c52b2281d5ede9df996b54c27b958fbeaac573b1
Faria-Lorin <florin20@stuy.edu>
2020-12-13 23:02:23 -0500

    I added an add method to override the add method in ArrayList class so that the method checks for null and only adds an element to the list if it is not null

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,9 @@
+import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
-  
+  public boolean add(T element){
+    if (element == null){
+      throw new IllegalArgumentException("IllegalArgumentException no nulls allowed");
+    }
+    return super.add(element);
+  }
 }
