ac7281b1b0710465aed239bbdc0cc4fb882a447d
Faria-Lorin <florin20@stuy.edu>
2020-12-14 06:36:38 -0500

    I added the add(index) method, which overlooks the given index and add the element according to the order of elements. I also added conditions for null elements

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,27 +9,49 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
-  private void indexFinder(T element){
+private void indexFinder(T element){
+    sortedIndex = 0;
     for ( int i = 0; i < super.size() - 1; i++){
-      if ((this.get(i).compareTo(element) < 0 && this.get(i+1).compareTo(element) > 0) || (this.get(i).compareTo(element) == 0)){
-        sortedIndex = i+1;
-      }
-    }
-  }
+          if ((this.get(i).compareTo(element) < 0 && this.get(i+1).compareTo(element) > 0) || (this.get(i).compareTo(element) == 0)){
+            sortedIndex = i+1;
+          }
+        }
+}
 
   public boolean add(T element){
-    indexFinder(element);
-    add(sortedIndex, element);
-    if (this.get(sortedIndex) == element){
+    if (element == null){
+      throw new IllegalArgumentException("No Nulls");
+    }
+    else{
+      indexFinder(element);
+      super.add(sortedIndex, element);
+    }
+    if( super.get(sortedIndex) == element){
       return true;
     }
     return false;
   }
 
+  public void add(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException("No Nulls");
+    }
+    else{
+      indexFinder(element);
+      super.add(sortedIndex, element);
+    }
+  }
+
+
   public T set(int index, T element){
-    T temp = this.get(index);
-    this.remove(index);
-    this.add(element);
-    return temp;
+    if (element == null){
+      throw new IllegalArgumentException("No Nulls");
+    }
+    else{
+      T temp = super.get(index);
+      super.remove(index);
+      super.add(element);
+      return temp;
+    }
   }
 }
