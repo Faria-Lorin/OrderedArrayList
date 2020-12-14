000733f7baea4ee22b78b9a17b440c618f7730cd
Faria-Lorin <florin20@stuy.edu>
2020-12-13 23:06:36 -0500

    I used the code I wrote for the add method and changed the return type and inputs to modify the code to override add(index) from ArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -2,8 +2,16 @@ import java.util.*;
 public class NoNullArrayList<T> extends ArrayList<T>{
   public boolean add(T element){
     if (element == null){
-      throw new IllegalArgumentException("IllegalArgumentException no nulls allowed");
+      throw new IllegalArgumentException("IllegalArgumentException NO nulls allowed");
     }
     return super.add(element);
   }
+
+  public void add(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException("IllegalArgumentException NO nulls allowed");
+    }
+    super.add(index,element);
+  }
+
 }
